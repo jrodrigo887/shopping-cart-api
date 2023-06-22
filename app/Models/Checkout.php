47ca1af3118444329511e604269561ae2b4f3fa9
joao <jrodrigo887@gmail.com>
2023-06-21 22:33:10 -0300

    feat: model checkout

---
+++ app/Models/Checkout.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Checkout extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'total',
+    ];
+
+    public function gettotalAttribute() {
+        return $this->attributes['total'] / 100; // 2990 => 29.9
+    }
+    public function settotalAttribute($attr) {
+        return $this->attributes['total'] = $attr * 100;
+    }
+
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Products::class);
+    }
+}
