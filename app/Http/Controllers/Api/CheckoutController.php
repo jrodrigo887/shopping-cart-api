27a48093ed2295011b1a080e411c34b9b1da9030
joao <jrodrigo887@gmail.com>
2023-06-22 14:33:25 -0300

    refactor: Complementando as controllers

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -19,34 +19,8 @@ class CheckoutController extends Controller
      */
     public function index()
     {
-        return $this->checkout::all();
-    }
-
-    public function addProductsTocheckout(Request $request, $checkoutId) {
-        $checkout = Checkout::find($checkoutId);
-
-        if (!$checkout) {
-            return response()->json(['message' => 'Checout não encontrado'], 404);
-        }
-
-        $prodsData = $request->input('products',[]);
-
-        $products = [];
-        foreach ($prodsData as $prod) {
-            $product = new Products($prod);
-            $products[] = $product;
-        }
-
-        $checkout->products()->saveMany($products);
-        return response()->json(['message' => 'Produtos adicionados com sucesso'], 200);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        $ckts = $this->checkout::withd('products')->get();
+        return response()->json($ckts);
     }
 
     /**
@@ -54,19 +28,31 @@ class CheckoutController extends Controller
      */
     public function show(string $id)
     {
-        $checkout = Checkout::find($id);
+        $ckt = $this->checkout::with('products')->find($id);
 
-        if (!$checkout) {
+        if (!$ckt) {
             return response()->json(['message' => 'Checkout não encontrado'], 404);
         }
 
-        $products[] = $checkout->getAssociatedProducts();
+        return response()->json($ckt, 200);
 
-        if (!$products) {
-            return response()->json(['message' => 'sem produtos'], 404);
-        }
 
-        return response()->json(['checkout' => $checkout, 'products' => $products], 200);
+        // $products[] = $checkout->getAssociatedProducts();
+
+        // if (!$products) {
+        //     return response()->json(['message' => 'sem produtos'], 404);
+        // }
+
+        // return response()->json(['checkout' => $checkout, 'products' => $products], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->checkout::created($request->all());
+        return response()->json(['message'=> 'Carrrinho Criado com sucesso.'], 201);
     }
 
     /**
@@ -74,7 +60,10 @@ class CheckoutController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data = $this->checkout::find($id);
+        $data->update($request->all());
+
+        return response()->json($data, 200);
     }
 
     /**
@@ -82,6 +71,38 @@ class CheckoutController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $this->checkout::destroy($id);
+        return response()->json(['message' => 'Checkout deleted']);
+    }
+
+    public function addProducts(Request $request, $checkoutId) {
+        $checkout = $this->checkout::find($checkoutId);
+
+        if (!$checkout) {
+            return response()->json(['message' => 'Checout não encontrado'], 404);
+        }
+
+        $products = $request->input('products',[]);
+        foreach ($products as $prod) {
+            $checkout->products()->created($prod);
+        }
+
+        // $checkout->products()->saveMany($products);
+        return response()->json(['message' => 'Produtos adicionados com sucesso', 'products'=> $products]);
+    }
+    public function removeProduct(Request $request, $checkoutId, $productId) {
+
+        $checkout = $this->checkout::find($checkoutId);
+        if (!$checkout) {
+            return response()->json(['message' => 'Checkout não encontrado'], 404);
+        }
+
+        $product = $this->checkout->products()->find($productId);
+        if (!$product) {
+            return response()->json(['message' => 'produto não encontrado.'], 404);
+        }
+
+        $product->delete();
+        return response()->json(['message' => 'Produtos removido com sucesso']);
     }
 }
--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -8,17 +8,17 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    private $prod;
+    private $product;
     function __construct(Products $products)
     {
-        $this->prod = $products;
+        $this->product = $products;
     }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return $this->prod::all();
+        return response()->json($this->product::all());
     }
 
     /**
@@ -26,7 +26,15 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validData = $request->validate([
+            'name'=> 'required|max:255',
+            'description'=> 'nullable',
+            'price'=> 'required|numeric|min:0',
+            'checkout_id'=> 'nullable',
+        ]);
+
+        $this->product::created($validData);
+        return response()->json(['message'=> 'Produto criado com sucesso.'], 201);
     }
 
     /**
@@ -34,7 +42,8 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $prod = $this->product::find($id);
+        return response()->json($prod);
     }
 
     /**
@@ -42,7 +51,16 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+
+        $validData = $request->validate([
+            'name'=> 'required|max:255',
+            'description'=> 'nullable',
+            'price'=> 'required|numeric|min:0',
+            'checkout_id'=> 'nullable',
+        ]);
+
+        $this->product::updated($validData);
+        return response()->json(200);
     }
 
     /**
@@ -50,6 +68,7 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $this->product::destroy($id);
+        return response()->json(['message'=> 'Produto deletado com sucesso.'], 200);
     }
 }
