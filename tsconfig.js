4a84eefa38fa1c1ad019909db3443946d5a6a4f5
joao <jrodrigo887@gmail.com>
2023-06-24 23:15:22 -0300

    style: resolva o modulo ts

--- tsconfig.js
+++ tsconfig.js
@@ -29,12 +29,13 @@ module.exports = {
         "skipLibCheck": true
     },
     "include": [
-        "resources/js/**/*.js",
-        "resources/ts/**/*.ts",
-        "resources/js/**/*.d.ts",
-        "resources/ts/**/*.d.ts",
-        "resources/**/*.tsx",
-        "resources/**/*.vue"
+        "./resources/js/**/*.js",
+        "./resources/ts/**/*.ts",
+        "./resources/ts/types/*.ts",
+        "./resources/js/**/*.d.ts",
+        "./resources/ts/**/*.d.ts",
+        "./resources/**/*.tsx",
+        "./resources/**/*.vue"
     ],
     "exclude": [
         "node_modules"
