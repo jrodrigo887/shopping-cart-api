91fde2391726c7d21036c3b7e8c825b0efe3d01d
joao <jrodrigo887@gmail.com>
2023-06-24 23:16:35 -0300

    feat: iniciando os componentes checkout e product

---
+++ resources/ts/components/CheckoutView.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+</script>
+<template>
+    <div>
+        <div>
+            <span>quantidade</span>
+            <p>produto</p>
+            <i>trash</i>
+        </div>
+        <button>
+            <i>cart</i>
+            <span>Fechar pedido</span>
+        </button>
+    </div>
+</template>
---
+++ resources/ts/components/ProductView.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <div class="flex space-x-3">
+        <button>Add</button>
+        <p>Nome</p>
+    </div>
+</template>
